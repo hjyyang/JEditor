67202f1656afb228846ea7f481ffc82f49e0f3c7
jon <13560586496@163.com>
2020-09-22 22:33:27 +0800

    feat: 添加icon

--- src/components/toolbar-left.vue
+++ src/components/toolbar-left.vue
@@ -1,6 +1,24 @@
 <template>
 	<div class="editor-toolbar-left">
-		<button>aaa</button>
+		<button>
+			<i class="iconfont icon-cuti"></i>
+		</button>
+		<i class="iconfont icon-liebiao-copy"></i>
+		<i class="iconfont icon-fangda"></i>
+		<i class="iconfont icon-lianjie"></i>
+		<i class="iconfont icon-icon-quote"></i>
+		<i class="iconfont icon-yanse"></i>
+		<i class="iconfont icon-xt"></i>
+		<i class="iconfont icon-tupian"></i>
+		<i class="iconfont icon-biaoti"></i>
+		<i class="iconfont icon-icon-shuziliebiao"></i>
+		<i class="iconfont icon-zhonghuaxian"></i>
+		<i class="iconfont icon-youcezhankaikanbujian"></i>
+		<i class="iconfont icon-xiahuaxian"></i>
+		<i class="iconfont icon-daima"></i>
+		<i class="iconfont icon-biaoji"></i>
+		<i class="iconfont icon-suoxiao"></i>
+		<i class="iconfont icon-yanjing"></i>
 	</div>
 </template>
 
