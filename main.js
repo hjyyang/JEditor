32361a3cb44db9ebadebe996fec397d916fd4d8a
huangjingyang <huangjingyang@mozat.com>
2020-10-09 15:51:30 +0800

    refactor: 删除不需要文件

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-// import Vue from "vue";
-// import App from "./App.vue";
-// import "./src/css/main.scss";
-// Vue.config.productionTip = false;
-
-// new Vue({
-// 	el: "#app",
-// 	render: (h) => h(App),
-// });
-
-import JEditor from "./JEditor.vue";
+import "./JEditor.vue";
